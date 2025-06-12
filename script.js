c72bf278e1b42536485f00b804c793c1137d2119
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2025-06-13 01:13:41 +0330

    object

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+
+//primitve(value) types:
+//string , number , bool , undifined , null
+
+//refrence type: opject , array , function
+let name = "fati";
+let person ={
+    name : "fati" 
+}
+console.log(person.name)
+console.log(person["name"])
+
+
+// let nameUser = "neon";
 console.log("neon");
 console.log(2+2);
