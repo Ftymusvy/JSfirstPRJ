b2d78c7c0e21b031c3d8d82f657e5e2de6104c50
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2025-06-14 16:14:12 +0330

    authenticated

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 
-//primitve(value) types:
-//string , number , bool , undifined , null
+// //primitve(value) types:
+// //string , number , bool , undifined , null
 
-//refrence type: opject , array , function
-let name = "fati";
-let person ={
-    name : "fati" 
-}
-console.log(person.name);
-console.log(person["name"]);
+// //refrence type: opject , array , function
+// let name = "fati";
+// let person ={
+//     name : "fati" 
+// }
+// console.log(person.name);
+// console.log(person["name"]);
 
-// array
-let users = ["ali" ,"fatma"];
-users[2]= 2
-console.log(users[0]);
+// // array
+// let users = ["ali" ,"fatma"];
+// users[2]= 2
+// console.log(users[0]);
 
-//function
-function greet(name){
-    console.log('Hello  ' + name);
-}
+// //function
+// function greet(name){
+//     console.log('Hello  ' + name);
+// }
 
-greet("ahmad");
-// let nameUser = "neon";
-console.log("neon");
-console.log(2+2);
+// greet("ahmad");
+// // let nameUser = "neon";
+// console.log("neon");
+// console.log(2+2);
 
-
+let authenticated = false;
+let bottonText = authenticated === true ? "dashboard" : "login" ;
+console.log(bottonText)
 
