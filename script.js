3da567ca9f8dfe76743fbe38c37c686de75df082
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2025-06-14 18:58:01 +0330

    OOP(object oriented programing)

--- script.js
+++ script.js
@@ -1,71 +1,85 @@
 
-// // //primitve(value) types:
-// // //string , number , bool , undifined , null
+// // // //primitve(value) types:
+// // // //string , number , bool , undifined , null
 
-// // //refrence type: opject , array , function
-// // let name = "fati";
-// // let person ={
-// //     name : "fati" 
+// // // //refrence type: opject , array , function
+// // // let name = "fati";
+// // // let person ={
+// // //     name : "fati" 
+// // // }
+// // // console.log(person.name);
+// // // console.log(person["name"]);
+
+// // // // array
+// // // let users = ["ali" ,"fatma"];
+// // // users[2]= 2
+// // // console.log(users[0]);
+
+// // // //function
+// // // function greet(name){
+// // //     console.log('Hello  ' + name);
+// // // }
+
+// // // greet("ahmad");
+// // // // let nameUser = "neon";
+// // // console.log("neon");
+// // // console.log(2+2);
+
+// // let authenticated = false;
+// // let bottonText = authenticated === true ? "dashboard" : "login" ;
+// // console.log(bottonText)
+
+// // //logical AND
+// // // &&
+// // let ProductCount = 3;
+// // let ProductPrice = 1000;
+// // let credit = 1200;
+// // let buy = ProductCount > 0 && credit>ProductPrice;
+
+// // console.log(buy);
+// // //logical OR
+// // // not
+
+// // // if 
+// // let hour = 12;
+// //  if (hour>=6 && hour<12) {   
+// //         console.log('good morning');
+// // }else 
+// // console.log('hello');
+
+// // // switch case
+
+// // let role = "guest";
+
+// // switch (role) {
+// //     case "guest": 
+// //         console.log('wellcome');
+// //         break;
+// //     case "admin": 
+// //         console.log('admin user');
+// //         break;
+
+// //     default:
+// //         console.log('unknown  user');  
 // // }
-// // console.log(person.name);
-// // console.log(person["name"]);
 
-// // // array
-// // let users = ["ali" ,"fatma"];
-// // users[2]= 2
-// // console.log(users[0]);
+// let person = { name: "Ali", age: 25, city: "Tehran" };
 
-// // //function
-// // function greet(name){
-// //     console.log('Hello  ' + name);
-// // }
-
-// // greet("ahmad");
-// // // let nameUser = "neon";
-// // console.log("neon");
-// // console.log(2+2);
-
-// let authenticated = false;
-// let bottonText = authenticated === true ? "dashboard" : "login" ;
-// console.log(bottonText)
-
-// //logical AND
-// // &&
-// let ProductCount = 3;
-// let ProductPrice = 1000;
-// let credit = 1200;
-// let buy = ProductCount > 0 && credit>ProductPrice;
-
-// console.log(buy);
-// //logical OR
-// // not
-
-// // if 
-// let hour = 12;
-//  if (hour>=6 && hour<12) {   
-//         console.log('good morning');
-// }else 
-// console.log('hello');
-
-// // switch case
-
-// let role = "guest";
-
-// switch (role) {
-//     case "guest": 
-//         console.log('wellcome');
-//         break;
-//     case "admin": 
-//         console.log('admin user');
-//         break;
-
-//     default:
-//         console.log('unknown  user');  
+// console.log("for...in:");//object iterable نیست
+// for (let key in person) {
+//   console.log(key + ": " + person[key]);
 // }
 
-let person = { name: "Ali", age: 25, city: "Tehran" };
+//object oriented programing
+let circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function(){console.log('draw')}
+};
 
-console.log("for...in:");//object iterable نیست
-for (let key in person) {
-  console.log(key + ": " + person[key]);
-}
+
+circle.draw();
